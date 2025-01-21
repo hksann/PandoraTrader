41a186b15393ec2616dbf758b6bc7dd3265ec538
hksann <96939706+hksann@users.noreply.github.com>
2025-01-21 18:56:41 +0800

    Update PandoraSimulator.cpp

--- PandoraSimulator/PandoraSimulator.cpp
+++ PandoraSimulator/PandoraSimulator.cpp
@@ -1,17 +1,19 @@
-// cwPegasusSimulator.cpp : Defines the entry point for the  Pandora Simulator console application.
+// cwPegasusSimulator.cpp : Defines the entry point for the Pandora Simulator console application.
 //
-//For more information, please visit https://github.com/pegasusTrader/PandoraTrader
+// For more information, please visit https://github.com/pegasusTrader/PandoraTrader
 //
-//≥˝∂‘Õ‚œ˙ €£¨√‚∑—Ã·π©£¨ª∂”≠∫œπÊ π”√
+// Èô§ÂØπÂ§ñÈîÄÂîÆÔºåÂÖçË¥πÊèê‰æõÔºåÊ¨¢ËøéÂêàËßÑ‰ΩøÁî®
 //
-//Please use the platform with legal and regulatory permission.
-//This software is released into the public domain.You are free to use it in any way you like, except that you may not sell this source code.
-//This software is provided "as is" with no expressed or implied warranty.I accept no liability for any damage or loss of business that this software may cause.
+// Please use the platform with legal and regulatory permission.
+// This software is released into the public domain. You are free to use it in any way you like, except that you may not sell this source code.
+// This software is provided "as is" with no expressed or implied warranty. I accept no liability for any damage or loss of business that this software may cause.
 //
 
-//#include "stdafx.h"
-
-//#define REALTIME_QUOTE
+#include <string>
+#include <cstdint> // or <stdint.h>
+#include <cmath>   // for pow, sqrt
+#include <unistd.h>  // for readlink
+#include <cstring>   // for memset
 
 #include "cwPegasusSimulator.h"
 
@@ -26,121 +28,98 @@
 #include "cwBasicCout.h"
 #include "cwVersion.h"
 
-#ifdef _MSC_VER
-#pragma comment(lib, "cwPandoraDLL.lib")
-#pragma comment(lib, "PandoraStrategy.lib")
-#pragma comment(lib, "tinyxml.lib")
-#endif // WIN32
+cwBasicCout             m_cwShow;
 
-cwBasicCout				m_cwShow;
+cwFtdcBrokerIDType      m_szMdBrokerID;
+cwFtdcUserIDType        m_szMdUserID;
+cwFtdcPasswordType      m_szMdPassWord;
 
-cwFtdcBrokerIDType		m_szMdBrokerID;
-cwFtdcUserIDType		m_szMdUserID;
-cwFtdcPasswordType		m_szMdPassWord;
-
-cwPegasusSimulator		m_PegasusSimulator;
+cwPegasusSimulator      m_PegasusSimulator;
 
 #ifdef REALTIME_QUOTE
-cwFtdMdSpi				m_mdCollector;
+cwFtdMdSpi              m_mdCollector;
 #else
-cwSimMdSpi				m_mdCollector;
+cwSimMdSpi              m_mdCollector;
 #endif // REALTIME_QUOTE
 
-cwSimTradeSpi			m_TradeChannel;
-cwStrategyDemo			m_Strategy;
+cwSimTradeSpi           m_TradeChannel;
+cwStrategyDemo          m_Strategy;
 
 std::vector<std::string> m_SubscribeInstrument;
 
-std::string				m_strStrategyConfigFile;
+std::string             m_strStrategyConfigFile;
 
 unsigned int PriceServerThread()
 {
-
-	m_TradeChannel.Connect(&m_PegasusSimulator);
+    m_TradeChannel.Connect(&m_PegasusSimulator);
 
 #ifdef REALTIME_QUOTE
-	m_mdCollector.m_bNoUseBasicMdUpdate = true;
+    m_mdCollector.m_bNoUseBasicMdUpdate = true;
 #endif
 
-	m_mdCollector.SetUserLoginField(m_szMdBrokerID, m_szMdUserID, m_szMdPassWord);
-	m_mdCollector.SubscribeMarketData(m_SubscribeInstrument);
+    m_mdCollector.SetUserLoginField(m_szMdBrokerID, m_szMdUserID, m_szMdPassWord);
+    m_mdCollector.SubscribeMarketData(m_SubscribeInstrument);
 
 #ifdef REALTIME_QUOTE
-	m_mdCollector.Connect("tcp://180.168.146.187:10131");
-	cwSleep(2000);
+    m_mdCollector.Connect("tcp://180.168.146.187:10131");
+    sleep(2);
 #else
-	m_mdCollector.Connect(&m_PegasusSimulator);
+    m_mdCollector.Connect(&m_PegasusSimulator);
 #endif // REALTIME_QUOTE
 
-	m_PegasusSimulator.SimulationStart();
+    m_PegasusSimulator.SimulationStart();
 
-	m_mdCollector.WaitForFinish();
+    m_mdCollector.WaitForFinish();
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	char exeFullPath[MAX_PATH];
-	memset(exeFullPath, 0, MAX_PATH);
-	std::string strFullPath;
-#ifdef WIN32
-	WCHAR TexeFullPath[MAX_PATH] = { 0 };
+    char exeFullPath[PATH_MAX];
+    memset(exeFullPath, 0, PATH_MAX);
+    std::string strFullPath;
 
-	GetModuleFileName(NULL, TexeFullPath, MAX_PATH);
-	int iLength;
-	//ªÒ»°◊÷Ω⁄≥§∂»   
-	iLength = WideCharToMultiByte(CP_ACP, 0, TexeFullPath, -1, NULL, 0, NULL, NULL);
-	//Ω´tchar÷µ∏≥∏¯_char    
-	WideCharToMultiByte(CP_ACP, 0, TexeFullPath, -1, exeFullPath, iLength, NULL, NULL);
-#else
-	size_t cnt = readlink("/proc/self/exe", exeFullPath, MAX_PATH);
-	if (cnt < 0 || cnt >= MAX_PATH)
-	{
-		printf("***Error***\n");
-		exit(-1);
-	}
-#endif // WIN32
+    ssize_t cnt = readlink("/proc/self/exe", exeFullPath, PATH_MAX);
+    if (cnt < 0 || cnt >= PATH_MAX)
+    {
+        printf("***Error***\n");
+        exit(-1);
+    }
 
-	strFullPath = exeFullPath;
-	strFullPath = strFullPath.substr(0, strFullPath.find_last_of("/\\"));
+    strFullPath = exeFullPath;
+    strFullPath = strFullPath.substr(0, strFullPath.find_last_of('/'));
 
-#ifdef WIN32
-	strFullPath.append("\\PandoraSimulatorConfig.xml");
-#else
-	strFullPath.append("/PandoraSimulatorConfig.xml");
-#endif // WIN32
+    strFullPath.append("/PandoraSimulatorConfig.xml");
 
+    m_Strategy.InitialStrategy(nullptr);
 
-	m_Strategy.InitialStrategy(nullptr);
-
-	m_PegasusSimulator.InitialSimulator(strFullPath.c_str());
+    m_PegasusSimulator.InitialSimulator(strFullPath.c_str());
 
 #ifdef REALTIME_QUOTE
-	m_PegasusSimulator.SetMdSpi((void*)dynamic_cast<cwBasicMdSpi*>(&m_mdCollector));
+    m_PegasusSimulator.SetMdSpi((void*)dynamic_cast<cwBasicMdSpi*>(&m_mdCollector));
 #else
-	m_PegasusSimulator.SetMdSpi((void*)(&m_mdCollector));
+    m_PegasusSimulator.SetMdSpi((void*)(&m_mdCollector));
 #endif // REALTIME_QUOTE
 
-	m_PegasusSimulator.SetTradeSpi((void*)&m_TradeChannel);
+    m_PegasusSimulator.SetTradeSpi((void*)&m_TradeChannel);
 
-	m_TradeChannel.RegisterBasicStrategy(dynamic_cast<cwBasicStrategy*>(&m_Strategy));
+    m_TradeChannel.RegisterBasicStrategy(dynamic_cast<cwBasicStrategy*>(&m_Strategy));
 
-	m_mdCollector.RegisterTradeSPI(dynamic_cast<cwBasicTradeSpi*>(&m_TradeChannel));
-	m_mdCollector.RegisterStrategy(dynamic_cast<cwBasicStrategy*>(&m_Strategy));
+    m_mdCollector.RegisterTradeSPI(dynamic_cast<cwBasicTradeSpi*>(&m_TradeChannel));
+    m_mdCollector.RegisterStrategy(dynamic_cast<cwBasicStrategy*>(&m_Strategy));
 
-	std::thread m_PriceServerThread = std::thread(PriceServerThread);
+    std::thread m_PriceServerThread = std::thread(PriceServerThread);
 
-	while (true)
-	{
-		cwSleep(3000);
-		m_cwShow.AddLog("%s %s »®“Ê:%.3f ∆Ω≤÷”Øø˜:%.3f ≥÷≤÷”Øø˜:%.3f",
-			m_PegasusSimulator.m_CurrentTradingDay,
-			m_PegasusSimulator.m_CurrentSimulationTime,
-			m_PegasusSimulator.m_cwSettlement.m_dBalance,
-			m_PegasusSimulator.m_cwSettlement.m_dCloseProfit,
-			m_PegasusSimulator.m_cwSettlement.m_dPositionProfit);
-	}
+    while (true)
+    {
+        sleep(3);
+        m_cwShow.AddLog("%s %s ÊùÉÁõä:%.3f Âπ≥‰ªìÁõà‰∫è:%.3f ÊåÅ‰ªìÁõà‰∫è:%.3f",
+            m_PegasusSimulator.m_CurrentTradingDay,
+            m_PegasusSimulator.m_CurrentSimulationTime,
+            m_PegasusSimulator.m_cwSettlement.m_dBalance,
+            m_PegasusSimulator.m_cwSettlement.m_dCloseProfit,
+            m_PegasusSimulator.m_cwSettlement.m_dPositionProfit);
+    }
     return 0;
 }
-
